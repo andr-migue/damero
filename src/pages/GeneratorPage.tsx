9e28fe3f3c24a71f8a73605d363bc7ee1b5209f4
andr-migue <miguelzamora210405@gmail.com>
2026-07-27 02:08:23 -0400

    feat: enhance GeneratorPage to dynamically generate QR codes based on user input

--- src/pages/GeneratorPage.tsx
+++ src/pages/GeneratorPage.tsx
@@ -1,24 +1,47 @@
 import { createParams } from "../core/params"
 import { render } from "../core/qr"
 import { useEffect, useState } from "react"
+import type { Params } from "../core/params"
 import './GeneratorPage.css'
 
 export function GeneratorPage() {
     const [src, setSrc] = useState<string>()
+    const [params, setParams] = useState<Params>(createParams(''))
+    const [draft, setDraft] = useState('')
 
     useEffect(
         () => {
-            let url: string
-            render(createParams('https://example.com')).then(blob => {
+            if (!params.data) return
+
+            let url: string | undefined
+
+            render(params).then(blob => {
                 url = URL.createObjectURL(blob)
                 setSrc(url)
             })
+
             return () => {
                 if (url) URL.revokeObjectURL(url)
             }
         },
-        []
+        [params]
     )
 
-    return src ? <img src={src} alt="qr" /> : <p>generando…</p>
+    return (
+        <div>
+            <form onSubmit={e => {
+                e.preventDefault()
+                setParams({ ...params, data: draft })
+            }}>
+                <input
+                    value={draft}
+                    onChange={e => setDraft(e.target.value)}
+                    placeholder="Type URL and press Enter"
+                />
+            </form>
+            {params.data && src
+                ? <img src={src} alt="qr" />
+                : <p>Type something to generate a QR Code.</p>}
+        </div>
+    )
 }
